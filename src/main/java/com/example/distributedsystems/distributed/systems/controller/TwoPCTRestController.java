a232f9a597ed5c9283d9227f7455dbe06ada1056
Nishtha Goswami <nishthagoswami@Nishthas-MacBook-Pro.local>
2023-04-27 23:25:13 -0400

    Mihir's TwoPC REST

---
+++ src/main/java/com/example/distributedsystems/distributed/systems/controller/TwoPCTRestController.java
@@ -0,0 +1,62 @@
+package com.example.distributedsystems.distributed.systems.controller;
+
+import com.example.distributedsystems.distributed.systems.dsalgo.twopc.TwoPCPromise;
+import com.example.distributedsystems.distributed.systems.model.Server;
+import com.example.distributedsystems.distributed.systems.model.user.User;
+import com.example.distributedsystems.distributed.systems.service.TwoPcRestService;
+import com.example.distributedsystems.distributed.systems.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"*"})
+@RequestMapping("/server")
+
+public class TwoPCTRestController {
+  private static final Logger logger = LoggerFactory.getLogger(TwoPCTRestController.class);
+
+  @Autowired
+  private TwoPcRestService twoPCRestService;
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/allServers")
+  public ResponseEntity<List<Server>> listServers() {
+    List<Server> servers = twoPCRestService.listAllServer();
+    return new ResponseEntity<>(servers, HttpStatus.OK);
+  }
+
+  @GetMapping("/ack")
+  public ResponseEntity<List<Server>> getAck() {
+    List<Server> servers = twoPCRestService.listAllServer();
+    return new ResponseEntity<>(servers, HttpStatus.OK);
+  }
+
+  // refactor
+  @GetMapping("/cancommit")
+  public ResponseEntity<Boolean> canCommit() {
+    return new ResponseEntity<>(true, HttpStatus.OK);
+  }
+
+  // refactor
+  @PostMapping("/docommit")
+  public ResponseEntity<TwoPCPromise> doCommit(@RequestBody User user) {
+    TwoPCPromise promise;
+    try {
+      promise = userService.createUser(user);
+    } catch (IllegalArgumentException exception) {
+      logger.info("Illegal Argument Exception: " + exception);
+
+      return new ResponseEntity<>(new TwoPCPromise(false,exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    return new ResponseEntity<>(promise, HttpStatus.OK);
+  }
+}
---
+++ src/main/java/com/example/distributedsystems/distributed/systems/service/TwoPCRestService.java
@@ -0,0 +1,22 @@
+package com.example.distributedsystems.distributed.systems.service;
+
+package com.example.distributedsystems.distributed.systems.service;
+
+import com.example.distributedsystems.distributed.systems.model.Server;
+
+import com.example.distributedsystems.distributed.systems.repository.TwoPCRestInterface;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TwoPcRestService {
+  @Autowired
+  private TwoPCRestInterface twoPCRestInterface;
+
+  public List<Server> listAllServer(){
+    return (List<Server>) twoPCRestInterface.findAll();
+  }
+}
